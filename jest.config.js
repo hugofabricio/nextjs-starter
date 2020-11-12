9dc1179441192e79723731c3348e13d89ec9e7d4
Hugo Fabricio <me@hugofabricio.com>
2020-11-12 07:52:44 -0300

    Fixed bug on jest.config and add ci action

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.test.ts(x)?'],
+  collectCoverageFrom: ['src/**/*.test.ts(x)?', '!src/**/*.stories.tsx'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/']
 }
